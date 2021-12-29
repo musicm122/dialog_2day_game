26da77b10f3c3dab891b99bc99eee03e7801e20a
musicm122 <terrance@hackerferret.com>
2021-12-29 00:42:54 -0500

    added title screen and character with movement

---
+++ GDUtils.cs
@@ -0,0 +1,40 @@
+﻿using Godot;
+using System;
+
+namespace Dialog2DayGame
+{
+    public class InputAction
+    {
+        public const String Shoot = "shoot";
+        public const String Left = "left";
+        public const String Right = "right";
+        public const String Up = "up";
+        public const String Down = "down";
+        public const String Pause = "pause";
+    }
+
+    public static class GDUtils
+    {
+        public static Vector2 GetTopDownMovementInput(float speed = 1f)
+        {
+            var velocity = Vector2.Zero;
+            if (Input.IsActionPressed(InputAction.Right))
+            {
+                velocity.x += 1f;
+            }
+            if (Input.IsActionPressed(InputAction.Left))
+            {
+                velocity.x -= 1f;
+            }
+            if (Input.IsActionPressed(InputAction.Up))
+            {
+                velocity.y -= 1f;
+            }
+            if (Input.IsActionPressed(InputAction.Down))
+            {
+                velocity.y += 1f;
+            }
+            return velocity.Normalized() * speed;
+        }
+    }
+}
